701bab5aea7ec771e3fb866ef274e153a202f5d7
vvn-sequeira <svivianmarcel@gmail.com>
2025-11-19 22:51:37 +0530

    First Commit

---
+++ GSAP/script.js
@@ -0,0 +1,56 @@
+
+// gsap.to("#box",{
+//     x:1000,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     repeat:-1,
+//     yoyo:true
+// })
+
+// gsap.from("#box2" , {
+//      x:1000,
+//      duration:2,
+//      delay:2
+// })
+
+
+// gsap.from("h1" , {
+//      opacity:0,
+//      duration:2,
+//      delay:1,
+//      y:20,
+//      stagger:1
+   
+// })
+
+
+var tl = gsap.timeline()
+
+// tl.to("#box",{
+//     x:1000,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+
+// })
+// tl.to("#box2",{
+//     x:1000,
+//     duration:2,
+//     rotate:360,
+
+// })
+
+
+tl.from(".Logo" , {
+     opacity:0,
+     y:-20,
+     duration:1.3,
+     
+})
+tl.from(".Links p" , {
+     opacity:0,
+     y:-30,
+     duration:1.3,
+     stagger:0.2
+})
